4fe0cacaa886c50576414d98a6249c0f1a9656c4
pr-iti <priti90066@gmail.com>
2025-08-17 23:59:34 +0530

    changes done in db to connect with atlas and add env to hide and add env to gitignore

--- db.js
+++ db.js
@@ -1,18 +1,27 @@
 // const mongoose = require('mongoose');
 
 //mongodb URL
-const mongoURL = 'mongodb://127.0.0.1:27017/hotels';
-
+// const mongoURL = 'mongodb://127.0.0.1:27017/hotels';
+//  const mongoURL = "mongodb+srv://priti:@priti0707@cluster0.nuvo5b8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+ // mongodb+srv://priti:<db_password>@cluster0.nuvo5b8.mongodb.net/
+ // mongodb+srv://priti:<db_password>@cluster0.nuvo5b8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 // set up mongodb connection
 // mongoose.connect(mongoURL,{
 //     useNewUriParser : true,
 //     useUnifiedtopology: true
-// })
-const mongoose = require("mongoose");
+// // })
+// const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/hotels")
-  .then(() => console.log(" MongoDB Connected"))
-  .catch((err) => console.error(" MongoDB Connection Error:", err));
+const mongoose = require('mongoose');
+//const mongoURL = 'mongodb://127.0.0.1:27017/hotels';
+
+require('dotenv').config();
+// const mongoURL = process.env.DB_URLlocal;
+  const mongoURL = process.env.DB_URL;
+
+mongoose.connect(mongoURL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB Connection Error:", err));
 
 
 const db = mongoose.connection;
--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const app = express();
 
+const PORT =process.env.PORT || 3000;
 const db = require('./db');
+require('dotenv').config();
 const Person = require('./Models/person');
 const menuItems = require('./Models/menu');
 const personRoutes = require('./routes/persons');
@@ -26,7 +28,7 @@ app.get('/', function(req, res)  {
 
 
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("server is listening");
 });
 
